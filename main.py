b6e4b712977e0273f5c3be627f9c8e2dd7bdc5b5
ajsdklf <140897315+ajsdklf@users.noreply.github.com>
2024-04-29 17:06:06 +0900

    ㅁㄴ

--- main.py
+++ main.py
@@ -8,11 +8,5 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 client = OpenAI(api_key=OPENAI_API_KEY)
 
-response = client.chat.completions.create(
-    model='gpt-3.5-turbo-0125',
-    messages=[
-        {'role': 'system', 'content': 'You are an assistant'}
-    ]
-)
+st.header("기록 MVP")
 
-print(response)
